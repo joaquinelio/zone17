7c7e3c6f01f9adcfcb80fa1045ce07246709d782
joaquinelio <joaquinelio@gmail.com>
2020-08-06 07:17:07 -0300

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # zone17
-reads issue17 from spanish js tutorial .then makes a list of abandoned articles order by date
+
+Reads issue17 from spanish js tutorial 
+
+.then makes a list of "abandoned" articles  (taken with no PR)
+
+
+Known bugs:
+None, I'm perfect.
+If article changes its name,
+If user changes login name,
+articles won't match. So the date won't show right.
+
+Future releases: Performance, css, toggle options, user feedback...
+Nah.
--- i17.js
+++ i17.js
@@ -1,66 +1,116 @@
 
-/*
-separar 
-  respissue
-  respcoment
-  mix -> htm
+//aArticles [ article, user, ]  
+//          filter by !done[X] +@user !#pr   
+//          no reorder      
+//aComments [ article, @user, date, id ]  
+//          article orderby, unique newer date    // no, no
+//          -filter by bot reaction-              // couldn't find    
 
+const urlIssue = "https://api.github.com/repos/javascript-tutorial/es.javascript.info/issues/17"
+const urlComments = "https://api.github.com/repos/javascript-tutorial/es.javascript.info/issues/17/comments?page="
 
-  regex!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
-  o vieja escuela 
-  mmm... awk es mas vieja escuela
+const artPR = 5
+const artUser = 4
+const artArt = 2
+const artDate = 6
 
-*/
-
+const comBody = 0
+const comUser = 1
+const comDate = 2
+const comId = 3
 
 
 async function lei17(){
 
-  const urlIssue = "https://api.github.com/repos/javascript-tutorial/es.javascript.info/issues/17"
-  const urlComments = "https://api.github.com/repos/javascript-tutorial/es.javascript.info/issues/17/comments?page="
+                  // *, [x], [article desc], (http:), (@user), #pr
+  const regIssue = /(\[[\sX]\])\s(\[.+\])\s?(\(http[.\S]+\))\s?(\(@\w+\))?\s?(#\d+)?/
+  //const regComments = /  /
 
-
-  let respIssue = await fetch(urlIssue)
-  let respComments = await fetch(urlComments)
-
+  //--------------------------------------------------------
+  let aArticles = await getArticles( urlIssue, regIssue  )
   
+  aArticles.shift()   // el 1ro es el ejemplo de Ilya
+  for (let aa of aArticles){aa.push("1492-10-12")}  // date 
+
+  //--------------------------------------------------------
+ let aComments = await getComments(urlComments)
+
+  //--------------------------------------------------------
+  for (let ac of aComments){
+    for (let aa of aArticles){
+
+      if(aa[artArt] === "[" + ac[comBody] + "]" && 
+          aa[artUser] == "(@"+ac[comUser]+")" && 
+          aa[artDate] < ac[comDate]) {
+
+              aa[artDate] = ac[comDate]  
+              //break ??
+      }
+    }
+  }
+
+  //--------------------------------------------------------
+  let sArticle="<p>"
+  for (let s of aArticles){ 
+    //s.shift()
+    //sArticle += "<hr><p>" + s.join("</p><p>") + "</p>"  
+    sArticle += "<hr><p>" + s[artArt] + "</p><p> " + s[artUser] + "</p><p> " + s[artDate] + "</p>"
+  }
+  document.getElementById("lista").innerHTML = 
+      aArticles.length + 
+      sArticle +
+      "</p><hr>"
+}
 
 
+async function getArticles(url, regexp){
+  let aArticles=[]
+  let respIssue = await fetch(url)
 
-  if (respIssue.ok){
+  if (respIssue.ok){               // .ok = http status ok
+
     let json = await respIssue.json()
-    let sBody = json.body
-    
-//    let aOldArticles = sBody.match(/^\* \[ ]    . ??????  ?(@/w+) ?????    ?!(#) ?????  \n$ ????? /g   ????????????????
 
-    let aOldArticles = sBody.split("\n").filter( ffOld )
+    aArticles = json.body.split("\n").filter((que)=>/^\* \[ ]/.test(que)) // No traducidos [X]  //.map( (s)=>s.match(regexp) )   
 
-//    alert (aOldArticles.length)
+  } else {
+    alert("resp issue BAAAAD") 
+  }
+  console.log(aArticles.shift().length )
 
-    document.getElementById("lista").innerHTML = aOldArticles.length + "<hr><p>" + aOldArticles.join("</p><p>") + "</p><hr>"
-
-   
-//    for(let i of aOldArticles){
-//	document.getElementById(" ").innerHTML = (
-//    }
-   
-
-
-
-  } else {alert("ufa!!!")}
-}
-
-function ffOld(que){
-
-  const regexp = /^\* \[ ]/
-
-  //  const regexp = /(^\* \[ ] )(\[[\w-]\])/     wtf 
-//  const regexp = /^(\* \[ ]) \[(/w+)] \((\w+)\) \((@/w+)\) \(?(!#)\)/ 
-
-  return regexp.test(que)
+  return aArticles.map( (s)=>s.match(regexp) ).filter((x)=>( x[artPR] == undefined && x[artUser] != undefined ) )     
 }
 
 
-async function getComments(){
+async function getComments(url){
+  let aComments = []
+  let pagina = 1
+  let respComments 
 
+  while (true){   // CAREFUL
+
+    respComments = await fetch(url+pagina)
+    if (respComments.ok){
+      let json = await respComments.json()
+
+      if (  typeof(json[0]) != "object"  ) {break} 
+
+      for (let o of json){
+        if (o.body.length < 55 ){
+          aComments.push([o.body, o.user.login, o.created_at, o.id])
+        }
+      }
+    } else {
+      alert("resp issue BAAAAD")
+      break 
+    } 
+    pagina++  
+  }
+  return aComments  
 }
+
+/*
+  regex!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+  o vieja escuela 
+  mmm... awk es mas vieja escuela
+*/
